db064e5c87733ef825d1031622a767b525cc3ec2
twen-24 <107887341+twen-24@users.noreply.github.com>
2023-08-03 13:30:18 -0400

    Update SmartWall.ino

--- SmartWall.ino
+++ SmartWall.ino
@@ -1,31 +1,31 @@
 int motorPin = 3;
 int sensorPin = 7;
-boolean wallClosed = true;
+boolean wallOpen = true;
 
 void setup() 
 { 
   pinMode(motorPin, OUTPUT);
   pinMode(sensorPin, INPUT);
   Serial.begin(9600);
-} 
- 
- 
+}
+
 void loop() 
 { 
-  int sensorData = digitalRead(sensorPin);
-  Serial.println(sensorData);
-
   //the sensor that we used displays boolean values, controlled by a potentiometer
-  if(sensorData == 1 && wallClosed){
+  int sensorData = digitalRead(sensorPin);
+
+  if(sensorData == 1 && wallOpen){
+    //spin wall open
     digitalWrite(motorPin, HIGH);
     delay(200);
     digitalWrite(motorPin,LOW);
-    wallClosed = false;
+    wallOpen = false;
   }
-  else if(sensorData==1 && !wallClosed) {
+  else if(sensorData == 0 && !wallOpen) {
+    //spin wall closed
     digitalWrite(motorPin, HIGH);
     delay(600);
     digitalWrite(motorPin,LOW);
-    wallClosed = true;
+    wallOpen = true;
   }
 }
